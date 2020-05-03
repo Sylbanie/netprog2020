9bcad137ef94c99f22641ad9e6c63cb45a3c90b7
Sylbanie <45379834+Sylbanie@users.noreply.github.com>
2020-05-03 13:11:54 +0700

    Create 02.practical.work.gethostbyname.c

---
+++ 02.practical.work.gethostbyname.c
@@ -0,0 +1,65 @@
+##Source Code C
+
+``
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
+  
+// Returns hostname for the local computer 
+void checkHostName(int hostname) 
+{ 
+    if (hostname == -1) 
+    { 
+        perror("gethostname"); 
+        exit(1); 
+    } 
+} 
+  
+// Returns host information corresponding to host name 
+void checkHostEntry(struct hostent * hostentry) 
+{ 
+    if (hostentry == NULL) 
+    { 
+        perror("gethostbyname"); 
+        exit(1); 
+    } 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    char hostbuffer[100]; 
+    struct hostent *host_entry; 
+    int hostname; 
+  
+    // To retrieve hostname 
+    hostname = gethostname(hostbuffer, sizeof(hostbuffer)); 
+    checkHostName(hostname); 
+  
+    // To retrieve host information 
+    host_entry = gethostbyname(hostbuffer); 
+    checkHostEntry(host_entry); 
+
+  
+    printf("Hostname: %s\n", hostbuffer); 
+  
+    return 0; 
+}
+
+##Complie in Ubuntu
+
+###Input Command
+``
+deensbn@ubuntu:~$ gcc /home/deensbn/Desktop/main.c -o main
+deensbn@ubuntu:~$ ./main
+``
+###Retrieve
+``
+Hostname: ubuntu
+``
