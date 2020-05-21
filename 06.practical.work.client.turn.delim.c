9e243be808177ff9779b38c62b63cffbac61698e
Sylbanie <45379834+Sylbanie@users.noreply.github.com>
2020-05-21 09:01:02 +0700

    Delimiter edited

--- 06.practical.work.client.turn.delim.c
+++ 06.practical.work.client.turn.delim.c
@@ -14,19 +14,19 @@ int main(int argc, char *argv[])
     char buffer[256];
     if (argc < 3) 
     {
-       fprintf(stderr,"Usage %s hostname port\n", argv[0]); \0
+       fprintf(stderr,"Usage %s hostname port\0", argv[0]);
        exit(0);
     }
     
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
-        error("ERROR opening socket"); \0
+        error("ERROR opening socket\0"); 
     
     server = gethostbyname(argv[1]);
     if (server == NULL) 
     {
-        fprintf(stderr,"ERROR, no such host\n"); \0
+        fprintf(stderr,"ERROR, no such host\0");
         exit(0);
     }
     
@@ -38,21 +38,21 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting"); \0
-    printf("Enter the message: "); \0
+        error("ERROR connecting\0");
+    printf("Enter the message: \0");
     
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
-         error("ERROR writing to socket"); \0
+         error("ERROR writing to socket\0");
     bzero(buffer,256);
     
     n = read(sockfd,buffer,255);
     if (n < 0) 
-         error("ERROR reading from socket"); \0
-    printf("%s\n",buffer); \0
+         error("ERROR reading from socket\0");
+    printf("%s\0",buffer);
     return 0;
 }
 
